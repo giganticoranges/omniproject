39eb5094e2131685113aa5954f2c9823322f7403
Spencer <spenceinvents@hotmail.com>
2019-10-30 00:19:53 -0400

    added strongest rssi filtering

--- trilateration-client/src/main.cpp
+++ trilateration-client/src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-#include <WiFi.h>
+//#include <WiFi.h>
 
 //bluetooth imports
 #include <BLEDevice.h>
@@ -21,23 +21,28 @@
 #define DEBUG_PRINT(x)
 #endif
 
+
 //bluetooth variables
 int scanTime = 1; //In seconds
 BLEScan *pBLEScan;
 
-const char *ssid = "aicv_devices";  //wifi network name
-const char *password = "AICVLab1!"; //wifi network password
+//const char *ssid = "aicv_devices";  //wifi network name
+//const char *password = "AICVLab1!"; //wifi network password
 
-const uint16_t port = 8090;         //port to connect to
-const char *host = "192.168.0.100"; //host ip address (ifconfig on the host pc)
+//const uint16_t port = 8090;         //port to connect to
+//const char *host = "192.168.0.100"; //host ip address (ifconfig on the host pc)
 
 int beaconArrayCount;
-int beaconsDataArray[MAX_BEACONS * 2];
+int beaconsIdArray[MAX_BEACONS]; //holds all the beacon ids found
+int beaconsRSSIArray[MAX_BEACONS]; //holds all rssi found
+int topBeaconsIDArray[3]; //holds the ids of the strongest 3 beacons recieved
+int topBeaconsRSSIArray[3]; //holds the RSSIs of the strongest 3 beacons recieved
 
 //function prototypes
 char *substring(char *string, int position, int length);
 void clearArray(int *arrayName, int numElements);
 String concatenateArray(int *arrayName, int numElements);
+void getStrongestThree(int *iDArray, int *rssiArray, int *strongestIDArray, int *strongestRSSIArray);
 
 class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
 {
@@ -46,24 +51,20 @@ class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
     DEBUG_PRINTLN("beaconArrayCount reset");
     int rssiRecieved = 0;
     //Serial.printf("Advertised Device: %s \n", advertisedDevice.toString().c_str());
-    if (advertisedDevice.haveRSSI())
-    {
+    if (advertisedDevice.haveRSSI()){
       rssiRecieved = (int)advertisedDevice.getRSSI();
     }
-    if (advertisedDevice.haveManufacturerData())
-    {
+    if (advertisedDevice.haveManufacturerData()){
       char *pHex = BLEUtils::buildHexData(nullptr, (uint8_t *)advertisedDevice.getManufacturerData().data(), advertisedDevice.getManufacturerData().length());
       char *major = substring(pHex, 41, 4);
       char *minor = substring(pHex, 45, 4); //don't really care which beacons are close (on client side)
-      if (strcmp(major, "1388") == 0)
-      {
-        if (beaconArrayCount / 2 < MAX_BEACONS)
-        {
+      if (strcmp(major, "1388") == 0){
+        if (beaconArrayCount < MAX_BEACONS){
           DEBUG_PRINT("beacon addded, beaconArrayCount: ");
           DEBUG_PRINTLN(beaconArrayCount);
-          beaconsDataArray[beaconArrayCount] = atoi(minor);
-          beaconsDataArray[beaconArrayCount + 1] = rssiRecieved;
-          beaconArrayCount += 2;
+          beaconsIdArray[beaconArrayCount] = atoi(minor);
+          beaconsRSSIArray[beaconArrayCount] = rssiRecieved;
+          beaconArrayCount += 1;
         }
       }
       free(pHex);
@@ -76,15 +77,14 @@ void setup()
 
   Serial.begin(115200); //set this serial rate in platformio.ini
 
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    DEBUG_PRINTLN("...");
-  }
+  //WiFi.begin(ssid, password);
+  //while (WiFi.status() != WL_CONNECTED){
+    //delay(500);
+    //DEBUG_PRINTLN("...");
+  //}
 
-  DEBUG_PRINT("WiFi connected with IP: ");
-  DEBUG_PRINTLN(WiFi.localIP());
+  //DEBUG_PRINT("WiFi connected with IP: ");
+  //DEBUG_PRINTLN(WiFi.localIP());
 
   //bluetooth stuff
   BLEDevice::init("");
@@ -92,65 +92,66 @@ void setup()
   pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
   pBLEScan->setActiveScan(true); //active scan uses more power, but get results faster
   pBLEScan->setInterval(100);
-  pBLEScan->setWindow(99); // less or equal setInterval value
+  pBLEScan->setWindow(95); // less or equal setInterval value
 }
 
 void loop()
 {
   //clear beacons array
-  clearArray(beaconsDataArray, beaconArrayCount);
+  clearArray(beaconsIdArray, beaconArrayCount);
+  clearArray(beaconsRSSIArray, beaconArrayCount);
+  clearArray(topBeaconsRSSIArray, 3);
+  clearArray(topBeaconsIDArray, 3);
+
   beaconArrayCount = 0;
 
   //bluetooth scan
-  BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
-  DEBUG_PRINT("Devices found: ");
-  DEBUG_PRINTLN(foundDevices.getCount());
+    BLEScanResults foundDevices = pBLEScan->start(1, false);
+    Serial.print("Devices found: ");
+    Serial.println(foundDevices.getCount());
 
   //create new wifi client
-  WiFiClient client;
-  if (!client.connect(host, port))
-  {
-    DEBUG_PRINTLN("Connection to host failed");
-    delay(1000);
-    return;
+  //WiFiClient client;
+  //if (!client.connect(host, port)){
+    //DEBUG_PRINTLN("Connection to host failed");
+    //delay(1000);
+    //return;
+  //}
+  //DEBUG_PRINTLN("Connected to server successful!");
+
+  getStrongestThree(beaconsIdArray, beaconsRSSIArray, topBeaconsIDArray, topBeaconsRSSIArray);
+  for (int x = 0; x < 3; x++){
+    Serial.print(x+1);
+    Serial.print(". Beacon ID: ");
+    Serial.print(topBeaconsIDArray[x]);
+    Serial.print("  Beacon RSSI: ");
+    Serial.println(topBeaconsRSSIArray[x]);
   }
-  DEBUG_PRINTLN("Connected to server successful!");
+    Serial.print("**\n");
 
-  Serial.println("beacons:");
-  for (int a = 0; a < beaconArrayCount; a++){
-    Serial.println(beaconsDataArray[a]);
-  }
+  //client.print(concatenateArray(beaconsIdArray, beaconArrayCount));
+  //client.print('\n');
 
-  client.print(concatenateArray(beaconsDataArray, beaconArrayCount));
-  client.print('\n');
-
-  /*for (int x = 0; x < beaconArrayCount; x++){
-    client.print(beaconsDataArray[x]);
-    client.print("\n");
-  }*/
   
-  DEBUG_PRINTLN("Disconnecting...");
-  client.stop();
+  //DEBUG_PRINTLN("Disconnecting...");
+  //client.stop();
 
   pBLEScan->clearResults(); // delete results fromBLEScan buffer to release memory
-  delay(200);
+  delay(50);
 }
 
-char *substring(char *string, int position, int length)
-{
+char *substring(char *string, int position, int length){
   char *pointer;
   int c;
 
   pointer = (char *)malloc(length + 1);
 
-  if (pointer == NULL)
-  {
+  if (pointer == NULL){
     DEBUG_PRINTLN("Unable to allocate memory.\n");
     exit(1);
   }
 
-  for (c = 0; c < length; c++)
-  {
+  for (c = 0; c < length; c++){
     *(pointer + c) = *(string + position - 1);
     string++;
   }
@@ -158,18 +159,16 @@ char *substring(char *string, int position, int length)
   return pointer;
 }
 
-void clearArray(int *arrayName, int numElements)
-{
+void clearArray(int *arrayName, int numElements){
   for (int x = 0; x < numElements; x++)
   {
-    Serial.println("abbout to reset: ");
-    Serial.println(arrayName[x]);
+    DEBUG_PRINTLN("abbout to reset: ");
+    DEBUG_PRINTLN(arrayName[x]);
     arrayName[x] = 0;
   }
 }
 
-String concatenateArray(int *arrayName, int numElements)
-{
+String concatenateArray(int *arrayName, int numElements){
   String toSend = "";
   for (int x = 0; x < numElements; x += 2){
     toSend += String(arrayName[x]);
@@ -177,7 +176,43 @@ String concatenateArray(int *arrayName, int numElements)
     toSend += String(arrayName[x + 1]);
     toSend += ',';
   }
-  Serial.print("Sending: ");
-  Serial.println(toSend);
+  //Serial.print("Sending: ");
+  //Serial.println(toSend);
   return toSend;
+}
+
+void getStrongestThree(int *iDArray, int *rssiArray, int *strongestIDArray, int *strongestRSSIArray){
+  int indexOfStrongest = -999;
+  int strongestRSSI = -999;
+
+  int indexOfSecondStrongest = -999;
+  int secondStrongestRSSI = -999; 
+
+  int indexOfThirdStrongest = -999;
+  int thirdStrongestRSSI = -999;
+
+  for(int x = 0; x < beaconArrayCount; x++){
+    if(rssiArray[x] > strongestRSSI){
+      strongestRSSI = rssiArray[x];
+      indexOfStrongest = iDArray[x];
+    }
+    else if(rssiArray[x] > secondStrongestRSSI){
+      secondStrongestRSSI = rssiArray[x];
+      indexOfSecondStrongest = iDArray[x];
+    }
+    else if(rssiArray[x] > thirdStrongestRSSI){
+      thirdStrongestRSSI = rssiArray[x];
+      indexOfThirdStrongest = iDArray[x];
+    }
+  }
+
+  strongestIDArray[0] = indexOfStrongest;
+  strongestRSSIArray[0] = strongestRSSI;
+
+  strongestIDArray[1] = indexOfSecondStrongest;
+  strongestRSSIArray[1] = secondStrongestRSSI;
+
+  strongestIDArray[2] = indexOfThirdStrongest;
+  strongestRSSIArray[2] = thirdStrongestRSSI;
+
 }
