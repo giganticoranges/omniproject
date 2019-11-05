68150737149a13901d33bf1bd5a83d4bc3da6b7d
Spencer <spenceinvents@hotmail.com>
2019-11-05 00:07:41 -0500

    updated scanning code
    more reliable scans, server side beacon sorting, plotting beacon locations

---
+++ trilateration-server/plot_test.py
@@ -0,0 +1,18 @@
+import sys
+from io import StringIO
+import pylab
+import pandas as pd
+import pandas
+
+x = 0.6
+y = 0.8
+
+df = pandas.read_csv('beacon_data.csv')
+print(df)
+
+for index, row in df.iterrows():
+    pylab.plot(row['x'], row['y'], 'rs')
+
+pylab.plot(x, y, 'bs')
+    
+pylab.show()
--- trilateration-server/server.py
+++ trilateration-server/server.py
@@ -1,4 +1,9 @@
 import socket
+import sys
+
+from io import StringIO
+
+import pandas as pd
 
 s = socket.socket() #create a new socket object
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -17,8 +22,12 @@ while True:
            break
  
         else:
-            signal_list = content.split(',')
-            print(signal_list)
+            #print(content)
+            DATA = StringIO(content.decode("utf-8")) #content stream is bytes, so cast to string
+            df = pd.read_csv(DATA, sep=",")
+            df.columns = df.columns.str.strip()
+            df = df.sort_values(by = 'RSSI', ascending = False)
+            print(df)
 
     #print("Client disconnectted - Closing connection") #if the client disconnects
     client.close()
